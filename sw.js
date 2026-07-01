ae9535ec706e98845bd56a856cb7229c215e2365
Nurtik <137826074+meladze-s@users.noreply.github.com>
2026-07-01 19:20:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'love-app-v2';
+const CACHE_NAME = 'love-app-v1';
 const ASSETS = ['index.html', 'style.css', 'script.js', 'manifest.json', 'icon.png'];
 
 self.addEventListener('install', e => {
